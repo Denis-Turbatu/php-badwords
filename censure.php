2641383745ec38d1abcae07d55c5efc5c3975301
Denis-Turbatu <turbatudenis81@gmail.com>
2024-05-20 14:55:06 +0200

    feat: censure word + style both php files

--- censure.php
+++ censure.php
@@ -1,15 +1,32 @@
-<?php 
-    $user_input = $_GET["userParagraph"];
+<?php
+$user_input = $_GET["user-paragraph"];
+$censure_word = $_GET["censure-word"];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Result</title>
 </head>
+
 <body>
-    <h1> <?php echo $user_input; ?> </h1>
+    <section class="w-50 mx-auto mt-5 border border-success">
+        <p class="mb-0 text-danger fs-3 fw-semibold">Paragrafo:</p>
+        <h1 class="fs-4 fw-semibold mb-4"> <?php echo $user_input; ?> </h1>
+        <p class="mb-0 text-danger fs-3 fw-semibold">Parola da censurare:</p>
+        <p class="fs-5"> <?php echo $censure_word; ?> </p>
+    </section>
+
+    <section class="w-50 mx-auto mt-5 border border-success">
+        <p class="mb-0 text-danger fs-3 fw-semibold">Paragrafo:</p>
+        <h1 class="fs-4 fw-semibold mb-4"> <?php echo $user_input; ?> </h1>
+        <p class="mb-0 text-danger fs-3 fw-semibold">Parola da censurare:</p>
+        <p class="fs-5"> <?php echo $censure_word; ?> </p>
+    </section>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Form</title>
 </head>
+
 <body>
-    <form action="censure.php" method="get">
-        <input type="text" name="userParagraph">
+    <form action="censure.php" method="get" class="d-flex flex-column w-25 mx-auto mt-5">
+        <label for="user-paragraph">Inserisci il paragrafo</label>
+        <textarea rows="4" cols="50" name="user-paragraph" class="mb-4"></textarea>
+        <label for="censure-word">Inserisci la parola da censurare</label>
+        <input type="text" name="censure-word" class="mb-4">
         <button type="submit">Invia</button>
     </form>
 </body>
+
 </html>
