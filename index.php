308455c66cf97357793304ff52808da792fbcbec
Denis-Turbatu <turbatudenis81@gmail.com>
2024-05-20 14:29:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form action="censure.php" method="get">
+        <input type="text" name="userParagraph">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
